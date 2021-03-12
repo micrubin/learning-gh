0702572bcd4d1c1d799e9c78def374b7cc1a014d
micah <micah.rubin2004@gmail.com>
2021-03-11 21:02:49 -0500

    Initial Commit

---
+++ gh/ghApp.swift
@@ -0,0 +1,17 @@
+//
+//  ghApp.swift
+//  gh
+//
+//  Created by Micah Rubin on 3/11/21.
+//
+
+import SwiftUI
+
+@main
+struct ghApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
